3102f10b0b5e580bf326af54e8eaa33f492254a5
Marlon Hubert <marlon.hubert@student.htw-berlin.de>
2025-06-15 11:00:19 +0200

    Farbiges Dreieck

--- src/triangle/Triangle.java
+++ src/triangle/Triangle.java
@@ -29,21 +29,11 @@ public class Triangle implements ResizableImage {
      * @return an Image containing the Triangle
      */
     private BufferedImage drawTriangle(Dimension size) {
-        print("drawTriangle: " + ++drawTriangle + "size: " + size);
+        print("drawTriangle: " + ++drawTriangle + " size: " + size);
         BufferedImage bufferedImage = new BufferedImage(size.width, size.height, BufferedImage.TYPE_INT_ARGB);
         Graphics2D gBuffer = (Graphics2D) bufferedImage.getGraphics();
 
-        gBuffer.setColor(Color.black);
-        int border = 2;
-        gBuffer.drawRect(border, border, size.width - 2 * border, size.height - 2 * border);
-
-        gBuffer.setColor(Color.darkGray);
-        border = 8;
-        gBuffer.drawRect(border, border, size.width - 2 * border, size.height - 2 * border);
-        gBuffer.drawString("Triangle goes here", border * 2, border * 4);
-
         int padding = 20;
-
         int maxWidth = size.width - 2 * padding;
         int maxHeight = size.height - 2 * padding;
 
@@ -57,32 +47,37 @@ public class Triangle implements ResizableImage {
         int[] B = {(int) (centerX - side / 2), (int) (topY + height)};
         int[] C = {(int) (centerX + side / 2), (int) (topY + height)};
 
-        int maxDepth = 6;
+        final int maxDepth = 6;
 
-        java.util.Stack<int[][]> stack = new java.util.Stack<>();
-        stack.push(new int[][]{A, B, C, new int[]{maxDepth}});
+        java.util.Stack<Object[]> stack = new java.util.Stack<>();
+        stack.push(new Object[]{A, B, C, 0});  // Ebene 0
 
         while (!stack.isEmpty()) {
-            int[][] item = stack.pop();
-            int[] top = item[0];
-            int[] left = item[1];
-            int[] right = item[2];
-            int depth = item[3][0];
+            Object[] item = stack.pop();
+            int[] top = (int[]) item[0];
+            int[] left = (int[]) item[1];
+            int[] right = (int[]) item[2];
+            int level = (int) item[3];
 
-            if (depth == 0) {
-                int[] xPoints = {top[0], left[0], right[0]};
-                int[] yPoints = {top[1], left[1], right[1]};
-                gBuffer.fillPolygon(xPoints, yPoints, 3);
-            } else {
+            // Farbe je nach Ebene
+            float hue = (float) level / maxDepth;
+            gBuffer.setColor(Color.getHSBColor(hue, 0.6f, 0.9f));
+
+            // Dreieck zeichnen
+            int[] xPoints = {top[0], left[0], right[0]};
+            int[] yPoints = {top[1], left[1], right[1]};
+            gBuffer.fillPolygon(xPoints, yPoints, 3);
+
+            if (level < maxDepth) {
                 int[] tl = {(top[0] + left[0]) / 2, (top[1] + left[1]) / 2};
                 int[] lr = {(left[0] + right[0]) / 2, (left[1] + right[1]) / 2};
                 int[] rt = {(right[0] + top[0]) / 2, (right[1] + top[1]) / 2};
 
-                int next = depth - 1;
+                int nextLevel = level + 1;
 
-                stack.push(new int[][]{top, tl, rt, new int[]{next}});
-                stack.push(new int[][]{tl, left, lr, new int[]{next}});
-                stack.push(new int[][]{rt, lr, right, new int[]{next}});
+                stack.push(new Object[]{top, tl, rt, nextLevel});
+                stack.push(new Object[]{tl, left, lr, nextLevel});
+                stack.push(new Object[]{rt, lr, right, nextLevel});
             }
         }
 
